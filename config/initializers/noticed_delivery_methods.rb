bbd3b6469eee0d8193693507ef3abf2fef2e320c
Simon Chiu <simon@furvur.com>
2026-01-01 22:27:53 -0500

    Fix Zeitwerk autoloading for GitHubDelivery class
    Rename github_delivery.rb to git_hub_delivery.rb to match the
    GitHubDelivery class name. Zeitwerk expects underscores between
    each capitalized word (git_hub not github).

--- config/initializers/noticed_delivery_methods.rb
+++ config/initializers/noticed_delivery_methods.rb
@@ -2,5 +2,5 @@
 Rails.application.config.to_prepare do
   require_relative '../../lib/noticed/delivery_methods/discord_delivery'
   require_relative '../../lib/noticed/delivery_methods/webhook_delivery'
-  require_relative '../../lib/noticed/delivery_methods/github_delivery'
+  require_relative '../../lib/noticed/delivery_methods/git_hub_delivery'
 end
